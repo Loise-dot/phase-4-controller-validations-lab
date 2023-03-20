c19611ffb3fc2f22d8bfa2bde7dca7c30d890a8f
Loise <loise.wambui@student.moringaschool.com>
2023-03-20 17:09:31 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, uniqueness: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: {maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    validate :title_include
+
+    def title_include
+
+     unless title && title.include?("Won't Believe" ||  "Secret" || "Top [number]" || "Guess")
+        errors.add(:title, "Title must be clickbait-y") 
+      end
+      # if title && !title.include?("Won't Believe" ||  "Secret" || "Top [number]" || "Guess")
+      #   errors.add(:title, "Title must be clickbait-y") 
+      # end
+    end
+
+
 end
